d3a2207b64b7245a926cd74f368ac7deb066bbb2
lakithaomal <you@example.com>
2020-12-01 12:24:41 -0600

    daily Updates

--- firmware/runDailyForAll.sh
+++ firmware/runDailyForAll.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-python3 liveRunDaily/syncWithPython.py liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 1 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 2 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 3 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 4 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 5 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 6 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 7 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 8 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 9 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 10 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 11 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 12 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 13 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 14 liveRunDaily/mintsDefinitions.yaml &
-./liveRunDaily/application/run_liveRunDaily.sh /home/teamlary/SW/v99 15 liveRunDaily/mintsDefinitions.yaml 
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 1 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 2 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 3 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 4 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 5 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 6 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 7 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 8 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 9 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 10 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 11 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 12 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 13 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 14 liveRunDynamic/mintsDefinitions.yaml &
+./liveRunDynamic/application/run_liveRunDynamic.sh /home/teamlary/SW/v99 15 liveRunDynamic/mintsDefinitions.yaml 
 
 echo "MINTS DONE :)"
 
