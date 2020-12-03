08ec71d9c06d6b70cbe33a91aa3004bf1fe3e83f
lakithaomal <you@example.com>
2020-12-03 11:16:58 -0600

    daily Updates

---
+++ firmware/runPlotterForAll.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "MINTS INIT ;)"
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 1 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 2 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 3 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 4 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 5 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 6 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 7 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 8 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 9 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 10 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 11 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 12 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 13 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 14 liveRunDynamic/mintsDefinitions.yaml
+./livePlotter/application/run_livePlotter.sh /home/teamlary/software/matlabRuntime/v99 15 liveRunDynamic/mintsDefinitions.yaml
+
+echo "MINTS DONE :)"
+
+
